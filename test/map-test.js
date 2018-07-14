76b44e73fbc68823254c0a7e88053fd94d8f81fa
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-07-13 19:52:38 -0700

    buffer-map

---
+++ test/map-test.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+const assert = require('./util/assert');
+const {BufferMap, BufferSet} = require('../lib/buffer-map');
+
+describe('buffer-map', function() {
+  it('should test map', () => {
+  });
+
+  it('should test set', () => {
+  });
+});
---
+++ work/buffer-map1.js
@@ -0,0 +1,172 @@
+/*!
+ * buffer-map.js - buffer map for javascript
+ * Copyright (c) 2018, Christopher Jeffrey (MIT License).
+ * https://github.com/bcoin-org/buffer-map
+ */
+
+'use strict';
+
+/**
+ * Buffer Map
+ */
+
+class BufferMap {
+  constructor() {
+    this.map = new Map();
+  }
+
+  get size() {
+    return this.map.size;
+  }
+
+  get(key) {
+    const item = this.map.get(toKey(key));
+
+    if (!item)
+      return undefined;
+
+    return item[1];
+  }
+
+  has(key) {
+    return this.map.has(toKey(key));
+  }
+
+  set(key, value) {
+    this.map.set(toKey(key), [key, value]);
+    return this;
+  }
+
+  delete(key) {
+    return this.map.delete(toKey(key));
+  }
+
+  clear() {
+    this.map.clear();
+  }
+
+  [Symbol.iterator]() {
+    return this.entries();
+  }
+
+  entries() {
+    return this.map.values();
+  }
+
+  *keys() {
+    for (const [key] of this.map.values())
+      yield key;
+  }
+
+  *values() {
+    for (const [, value] of this.map.values())
+      yield value;
+  }
+}
+
+/**
+ * Buffer Set
+ */
+
+class BufferSet {
+  constructor() {
+    this.map = new Map();
+  }
+
+  get size() {
+    return this.map.size;
+  }
+
+  has(key) {
+    return this.map.has(toKey(key));
+  }
+
+  add(key) {
+    this.map.set(toKey(key), key);
+    return this;
+  }
+
+  delete(key) {
+    return this.map.delete(toKey(key));
+  }
+
+  clear() {
+    this.map.clear();
+  }
+
+  [Symbol.iterator]() {
+    return this.keys();
+  }
+
+  *entries() {
+    for (const key of this.map.values())
+      yield [key, key];
+  }
+
+  keys() {
+    return this.map.values();
+  }
+
+  values() {
+    return this.map.values();
+  }
+}
+
+/*
+ * Helpers
+ */
+
+function toKey(key) {
+  if (!Buffer.isBuffer(key))
+    throw new TypeError('Non-buffer passed to buffer map/set.');
+
+  return key.toString('binary');
+}
+
+/*
+ * POST
+ */
+
+{
+  const assert = (ok) => {
+    if (!ok)
+      throw new Error('BufferMap: POST failed.');
+  };
+  {
+
+    const map = new BufferMap();
+    const key = Buffer.allocUnsafe(256);
+
+    for (let i = 0; i < 256; i++)
+      key[i] = i;
+
+    const copy = Buffer.from(key);
+
+    map.set(key, true);
+
+    assert(map.get(key) === true);
+    assert(map.get(copy) === true);
+  }
+
+  {
+    const map = new BufferMap();
+    const key = Buffer.allocUnsafe(64);
+
+    for (let i = 0; i < 64; i++)
+      key[i] = i;
+
+    const copy = Buffer.from(key);
+
+    map.set(key, true);
+
+    assert(map.get(key) === true);
+    assert(map.get(copy) === true);
+  }
+}
+
+/*
+ * Expose
+ */
+
+exports.BufferMap = BufferMap;
+exports.BufferSet = BufferSet;
